e4fbe09e0757b6e3cdaf66113aebbf751f02fbba
Joe H. Rahme <jhakimra@redhat.com>
2018-04-16 13:18:08 +0200

    Replaces filter with get_or_create()

--- core/views.py
+++ core/views.py
@@ -14,15 +14,13 @@ def index(request):
     return render(request, 'core/index.html', {'new_game_id': uuid.uuid1()})
 
 def display(request, game_id):
-    g = Game.objects.filter(game_uuid=game_id).first()
-    if g is None:
-        g = Game.objects.create(game_uuid=game_id)
+    g, created = Game.objects.get_or_create(game_uuid=game_id)
+    if created:
         market = PriceList.objects.create(name="Game {} - day 0".format(game_id), game=g)
         backpack = Backpack.objects.create(game=g)
         for i in Item.objects.all():
             ip = ItemPrice.objects.create(item=i, pricelist=market)
             iq = ItemQuantity.objects.create(item=i, quantity=0, backpack=backpack)
-
         
     return render(request, 'core/game.html',
                   {'game': g,
